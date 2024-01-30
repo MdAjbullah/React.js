b9750671714f419f8983b4ed72fc8f2b26956b13
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2024-01-30 22:30:34 +0530

    theory

---
+++ theory/basic.js
@@ -0,0 +1,70 @@
+/**
+ * javascript libaraies to bouild dynamic  and interactive user intrface
+ * it is develiped by facebook in 20011 
+ * it is currernt most widly used js labarary for frontend development
+ * used to create single page application.
+ * it is also know as spa 
+ * react is used to build reusable ui components
+ * 
+ */
+
+
+// working of DOM 
+/**
+ * DOM is a programming interface for html and xml documents
+ * it represents the page so that programs can change the document structure, style and content
+ * DOM represents the document as nodes and objects
+ * 
+ * browser takes html and create DOM.
+ * js helps us modify Dom based on user action or event
+ * in big application working with DOm becomes complex.
+ * react simplifies this by using virtual DOM
+ * 
+ */
+
+/**
+ * problem with javascropt
+ * react has a simpler mental model 
+ * jsic cumbersom to work with
+ * js is error prone 
+ * js is hard to maintain
+ * 
+ * 
+ * 
+ */
+
+
+/**
+ * working of react  
+ * no need to worry about quryin and updating the DOM
+ * react creat a web pagr with small reusable components
+ * react will take of creating and updatin dom element 
+ * it serves a lot of time cheezeing aasan hai phale likha=
+ */
+
+// js vs react
+/**
+ * js is a programming language
+ * react is a js library
+ * js is used to create a web page
+ * react is used to create a ui component
+ * js is used to create a single page application
+ * react is used to create a single page application
+ * 
+ * 
+ * 
+ * 
+ */
+
+// introduction to react component 
+
+/**
+ * components help us write reusable code and batter organized code. 
+ * root is aap component. 
+ * root component is a parent component.
+ * parent component can have child component.
+ * child component can have child component.
+ * 
+ * 
+ */
+
